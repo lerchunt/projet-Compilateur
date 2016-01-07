58bc4de0939bdaf8c1e88b6944073f48f6390330
jcata <j.catania.c@gmail.com>
2016-01-07 11:13:51 +0100

    màj backend

--- compproj/java/Backend.java
+++ compproj/java/Backend.java
@@ -1,11 +1,13 @@
 
 
 import java.io.*;
+import java.util.Hashtable;
 import java.util.LinkedList;
 
 public class Backend {
 	String pathFolder;
 	String nameFolder;
+	Hashtable<String,String> variables = new Hashtable<String,String>();
 	
 	public Backend (String path) { 
 
@@ -42,6 +44,7 @@ public class Backend {
 			buffer.close(); 
 			
 			int numLigne = 0;
+			int nbRegVar=3;
 			for (String[] l : strMinCaml){
 				// vérification 1ère ligne
 				if (numLigne == 0) {
@@ -50,18 +53,43 @@ public class Backend {
 					assert(l[2] == "=");
 					retour = String.format("%s\t%s\n%s\n", retour, ".global _start", "_start:");
 				} else {
-					// cas print_new_line
+					String toWrite="";
 					switch (l[0]) {
-						case "call":
-							String toWrite="";
-							if (l[1].substring(0, 9).equals("_min_caml")) {
-								toWrite = l[1].substring(1);
-								int lenght = l[1].length();
-								if (l[1].substring(lenght-2, lenght).equals("()")) {
-									toWrite = toWrite.substring(0, lenght-3);
-								}
-							} 
-							retour = String.format("%s\tbl\t%s\n", retour, toWrite );
+						case "call":	
+							if (l[1].length() >=9 ) {
+								if (l[1].substring(0, 9).equals("_min_caml")) {
+									int lenght = l[1].length();
+									if(l[1].substring(9, 19).equals("_print_int")){
+										String var = l[2];
+										String registre = variables.get(var);
+										toWrite = "\tmov\t"+"r0"+","+registre+"\n";
+									}
+
+									toWrite += "\tbl\t" +l[1].substring(1);
+									
+									lenght = l[1].length();
+									if (l[1].substring(lenght-2, lenght).equals("()")) {
+										toWrite = toWrite.substring(0, toWrite.length()-2);
+									}
+									
+									
+								} 
+							}
+							retour = String.format("%s%s\n", retour, toWrite );
+							toWrite = "";
+							break;
+						 case "let":
+							String Var = l[1];
+							nbRegVar++;
+							String num = l[3];
+							String registre = String.format("r%d", nbRegVar);
+							this.variables.put(Var, registre);
+							
+							toWrite = "mov\tr"+nbRegVar+",#"+num;
+						
+							retour = String.format("%s\t%s\n", retour, toWrite );
+							toWrite = "";
+							break;
 						default:
 					}
 					
@@ -80,5 +108,14 @@ public class Backend {
 		catch (Exception e){
 			System.out.println(e.toString());
 		}
+	}
+	
+	private void Affiche(String[] l){
+		System.out.print(l.length);
+		for (int i =0; i<l.length; i++) {
+			System.out.print(l[i]);
+			
+		}
+		System.out.print("\n");
 	}
 }
