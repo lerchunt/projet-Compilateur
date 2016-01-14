f3b9d29ade91293c9139e3c46ac2d6b6e26cc1db
lerchunt <lerchundi.thomas@gmail.com>
2016-01-14 09:33:22 +0100

    ajout des tests

---
+++ compproj/java/tests/TestsOk/test_if_let_bool_float_letrec.ml
@@ -0,0 +1,13 @@
+(* 1.0 6  *)
+let v1 = 1.3 in
+let v6 = 1.0 in 
+let v2 = 2.0 in 
+let v3 = v1 +. v2 in v3 ;
+let x = 1.0 in print_float x ;
+let v4 = 1.0 in 
+let v5 = v6 in 
+let a = true in
+let b = true in 
+if a = b then v4 else v5;
+let rec test x = x + 1 in 
+print_int(test 5) 
---
+++ compproj/java/tests/TestsOk/test_letrec2.ml
@@ -0,0 +1,6 @@
+let rec succ x = 
+	let z = 4 in
+	let  y = z in
+	let amodif = y + z in
+	5 + y in
+print_int (succ (2) );
