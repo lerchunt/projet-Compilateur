535eaa083d7d97a7e489e9ea17e5206c72f94782
apollinaire.desbiens <apollinaire.desbiens@gmail.com>
2016-01-26 11:56:28 +0100

    debug ordre sup

--- compproj/java/ConstantFolding.java
+++ compproj/java/ConstantFolding.java
@@ -180,9 +180,12 @@ public class ConstantFolding implements ObjVisitor<Exp> {
 			if(e.e1 instanceof Var){
 				if(tabSym.containsKey(((Var)e.e1).id.toString())){
 					tabVar.put(((Var)e.e1).id.toString(), true) ;
-					e.e1 = tabSym.get(((Var)e.e1).id.toString());
+					Exp retour = tabSym.get(((Var)e.e1).id.toString());
+					if (retour instanceof Var) {
+						e.e1 = retour;
+					} 
 				}
-			}
+			} 
 			e.e1 = e.e1.accept(this) ; 
 			if (e.e1 instanceof Int || e.e1 instanceof Float || e.e1 instanceof Bool || e.e1 instanceof Var || e.e1 instanceof Unit) {
 				tabSym.put(e.id.toString(), e.e1);
